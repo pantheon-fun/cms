ef24bf5f82291fdf77d96cc7c32d306796fc6c0c
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-05-13 01:01:53 +0300

    Repo init

---
+++ schemas/sections/about-us-section.js
@@ -0,0 +1,18 @@
+export default {
+  title: 'About us section',
+  name: 'aboutUsSection',
+  type: 'document',
+  fields: [
+    {
+      title: 'Заголовок',
+      name: 'heading',
+      type: 'string',
+    },
+    {
+      title: 'Описание',
+      description: 'Какой-то текст "о нас"',
+      name: 'text',
+      type: 'text',
+    },
+  ],
+};
